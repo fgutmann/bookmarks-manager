706ef68034fd59cc11d1c72ae411ff0ebf1f0a47
Florian Gutmann <f.gutmann@irregular.at>
2011-04-07 17:28:18 +0200

    Build nice runnable jar

--- src/main/java/com/chronimo/bookmarks/Delicious2Firefox.java
+++ src/main/java/com/chronimo/bookmarks/Delicious2Firefox.java
@@ -6,24 +6,28 @@ import java.io.IOException;
 import com.chronimo.bookmarks.delicious.DeliciousManager;
 import com.chronimo.bookmarks.firefox.FirefoxManager;
 
-public class Delecious2Firefox {
+public class Delicious2Firefox {
 	
 	public static void main(String [] args) throws IOException {
 		if (args.length < 3) {
-			System.err.println("Usage: java -jar delicious2firefox.jar <delicous.htm> <firefox.json> <outfile.json>");
+			System.err.println("Usage: java -jar delicious2firefox.jar <delicious .htm file> <firefox .json file> <new .json file>");
+			return;
 		}
 		
 		File deliciousFile = new File(args[0]);
 		if (!deliciousFile.exists() || !deliciousFile.canRead()) {
 			System.err.println("Can't read the delicious file.");
+			return;
 		}
 		File firefoxFile = new File(args[1]);
 		if (!firefoxFile.exists() || !firefoxFile.canRead()) {
 			System.err.println("Can't read the firefoxFile file.");
+			return;
 		}
 		File outFile = new File(args[2]);
-		if (!outFile.canWrite()) {
+		if ((outFile.exists() && !outFile.canWrite()) || (!outFile.exists() && !outFile.createNewFile())) {
 			System.err.println("Can't write to outFile.");
+			return;
 		}
 		
 		DeliciousManager delicious = new DeliciousManager();
